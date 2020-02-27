ed90577fc29ba08d1b48ab791516f0cf4d44a93f
Mladen Mikic <mb87.apple@gmail.com>
2020-02-27 09:57:43 +0100

    Initial Commit

---
+++ AudioPlayground/AudioPlayground.h
@@ -0,0 +1,19 @@
+//
+//  AudioPlayground.h
+//  AudioPlayground
+//
+//  Created by Mladen Mikic on 27/02/2020.
+//  Copyright © 2020 excellence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioPlayground.
+FOUNDATION_EXPORT double AudioPlaygroundVersionNumber;
+
+//! Project version string for AudioPlayground.
+FOUNDATION_EXPORT const unsigned char AudioPlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioPlayground/PublicHeader.h>
+
+
